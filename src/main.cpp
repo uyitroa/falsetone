b55eaae227a413a9c0279034fda9e3ce0488b826
shintaroDesu <snkraishin87@gmail.com>
2019-02-27 16:55:07 +0100

    optimize a little bit and rename to more useful meaning

--- src/main.cpp
+++ src/main.cpp
@@ -32,13 +32,18 @@ float calc() {
 	uint point_col = 0;
 	uint point_row = 0;
 	uint color = 0;
-	for (int i = 0; i < img.cols; i++) {
-		for (int j = 0; j < img.rows; j++) {
-			point_col = center_col - abs(center_col - i);
-			point_row = center_row - abs(center_row - j);
+
+	int col_begin = img.cols * 0.2;
+	int col_end = img.cols * 0.8;
+	int row_begin = img.rows * 0.2;
+	int row_end = img.rows * 0.8;
+	for (int i = col_begin; i < col_end; i++) {
+		for (int j = row_begin; j < row_end; j++) {
+			coefficient_col = center_col - abs(center_col - i);
+			coefficient_row = center_row - abs(center_row - j);
 			color = getint(&img, j, i);
-			sum_color += (color * (point_col + point_row)) / 10000;
-			max_color += (200 * (point_col + point_row)) / 1000;
+			sum_color += (color * (coefficient_col + coefficient_row)) / 10000;
+			max_color += (200 * (coefficient_col + coefficient_row)) / 1000;
 		}
 		//std::cout << sum_color << "\n";
 	}
